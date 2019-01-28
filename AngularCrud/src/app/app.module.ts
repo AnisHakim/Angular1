d071aad27e0e0ffed062e546b1e55097457b4d34
AnisHakim <hakimanis96@yahoo.fr>
2019-01-28 12:16:11 +0100

    first commit

---
+++ AngularCrud/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListEmployeesComponent } from './employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
+import { FormsModule }   from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListEmployeesComponent,
+    CreateEmployeeComponent,
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
